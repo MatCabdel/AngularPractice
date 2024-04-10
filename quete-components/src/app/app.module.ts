611f912f03ce2f304bcee7c091399a5c9f4f0674
Mathieu Chauveau <chauveau.math@gmail.com>
2024-04-10 10:50:06 +0200

    quête01 ok

---
+++ quete-components/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { BlockComponent } from './block/block.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    BlockComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ quete-components/src/app/block/block.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  templateUrl: './block.component.html',
+  styleUrl: './block.component.scss'
+})
+export class BlockComponent {
+  block: string = 'A B C'
+}
